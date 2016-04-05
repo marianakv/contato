f99cc31f850f15c7296636872bd4d96c7a33aa08
Mariana Vidal <marianakv@gmail.com>
2016-04-05 12:10:56 -0300

    Faltou concatenar
    Erro na escrita, faltou concatenar

--- contact.php
+++ contact.php
@@ -1,26 +1,23 @@
 <?php
-$field_name = $_POST['nome'] = ( isset($_POST['nome']) );
-$field_email = $_POST['email'] = ( isset($_POST['email']) );
-$field_telefone = $_POST['telefone'] = ( isset($_POST['telefone']) );
-$field_whatsapp = $_POST['whatsapp'] = ( isset($_POST['whatsapp']) ) ? true : null;
-
+$field_name = $_POST['nome'];
+$field_email = $_POST['email'];
+$field_telefone = $_POST['telefone'];
+$field_whatsapp = $_POST['whatsapp'];
 
 $mail_to = 'email@gmail.com';
 $subject = 'Teste' .$field_name;
 
 $body_message = 'Cadastro: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
-$body_message = 'Telefone: '.$field_telefone."\n";
-$body_message = 'whatsapp: '.$field_whatsapp;
+$body_message .= 'Telefone: '.$field_telefone."\n";
+$body_message .= 'whatsapp: '.$field_whatsapp;
 
-$headers = "MIME-Version: 1.1\r\n";
-$headers .="Content-type: text/plain; charset=iso-8859-1\r\n";
-$headers .= "From: $field_email\r\n";
-$headers .= "Return-Path: $field_email\r\n";
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
 
-$envio = mail($mail_to, $subject, $body_message, $headers);
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-if ($envio) { ?>
+if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 	alert('Obrigado pela mensagem. Entraremos em contato em breve');
 	window.location = 'index.html';
